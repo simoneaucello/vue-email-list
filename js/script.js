b728ee4d48778404080b107411259951e237ff59
simoneaucello <simoneaucello@gmail.com>
2024-03-19 14:36:33 +0100

    [add] api method

--- js/script.js
+++ js/script.js
@@ -12,11 +12,21 @@ createApp({
 
   methods: {
 
+    getApi(){
+      // creo un ciclo for per riempire l'array
+      for(let i = 0; i < 10; i++){
+      // attraverso l'api faccio un push dell'email generata nell'array emails  
+        axios.get(this.apiUrl).then( (res) => {
+          console.log(res.data.response);
+          this.emails.push(res.data.response)
+        })
+      }
+    }
 
   },
 
   mounted(){
-
+    this.getApi()
   }
 
 }).mount('#app')
